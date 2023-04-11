084f080a81fbb412e3973ee89843596c355b7449
Tania Ortiz <taniaortizdiaz@gmail.com>
2023-04-10 20:08:04 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { port } from './config/index.js';
+
+const app = express();
+
+app.get('/', (request, response, error) => {
+
+  response.send('status: OK')
+
+});
+
+app.listen(port, (error) => {
+
+  if(error) {
+    console.log('server Error: Failed');
+    process.exit(1);
+  }
+  console.log('server listening in port ' + port)
+})
